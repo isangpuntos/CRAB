166af1ee4b8df3f84d9cb8003e29047f3186e06b
isangpuntos <isang.puntos@gmail.com>
2017-09-03 17:42:57 +0800

    Add import requests
    Add import requests

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from flask import request
 from flask import make_response
 from lxml import html
 from io import StringIO, BytesIO
+import requests
 
 # Flask app should start in global layout
 app = Flask(__name__)
